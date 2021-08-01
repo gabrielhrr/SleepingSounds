693ffae72dc06d46020b847adb737ad6a3a753df
Gabriel <gabriehlr@gmail.com>
2021-08-01 15:25:15 -0300

    name and icon changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Xylophone App',
+      title: 'Sleeping Sounds',
       home: Scaffold(
         body: SafeArea(
           child: Column(
